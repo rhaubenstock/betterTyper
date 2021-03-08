c8dc8de1c916f084b02101d5118e7eb8fc882381
Reed Haubenstock <rhaubenstock@users.noreply.github.com>
2021-03-07 20:05:10 -0800

    Adds initial files

---
+++ typingTrial.js
@@ -0,0 +1,13 @@
+window.addEventListener("DOMContentLoaded", () => {
+  const referenceEl = document.getElementById("reference");
+  const inputEl = document.getElementById("input");
+  
+  const sampleText = "The quick brown fox jumped over the lazy dog.";
+
+  referenceEl.innerText = sampleText;
+
+});
+
+
+
+
