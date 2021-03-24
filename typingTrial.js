3203dc2a9677bf1b08aa6998cd794b7016ddd163
Reed Haubenstock <rhaubenstock@users.noreply.github.com>
2021-03-23 22:31:07 -0700

    Try out current char highlight

--- typingTrial.js
+++ typingTrial.js
@@ -1,12 +1,43 @@
+
+
+
+
 window.addEventListener("DOMContentLoaded", () => {
   const referenceEl = document.getElementById("reference");
   const inputEl = document.getElementById("input");
   
   const sampleText = "The quick brown fox jumped over the lazy dog.";
 
-  referenceEl.innerText = sampleText;
+  const doneText = "";
+  const currChar = sampleText[0];
+  const nextText = sampleText.slice(1);
+
+
+  const doneSpan = document.createElement("span");
+  doneSpan.innerText = doneText;
+  doneSpan.classList.add("correct");
+
+  const currSpan = document.createElement("span");
+  currSpan.innerText = currChar;
+  currSpan.classList.add("current");
+
+  const nextSpan = document.createElement("span");
+  nextSpan.innerText = nextText;
+  nextSpan.classList.add("next");
+
+  referenceEl.innerHTML="";
+  [doneSpan, currSpan, nextSpan].forEach(el => referenceEl.appendChild(el));
+
+
 
 });
+
+
+// next to do -- array of string + correct or wrong
+// add eventListener for keyDown on inputEl
+// add eventListener for keyUp on inputEl
+// be able to control text of inputEl
+// through the keyDown inputs
 
 
 
