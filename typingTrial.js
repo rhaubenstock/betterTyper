3a2a8a4e1d4b1c5b533463b6778ee81e4b58a04a
Reed Haubenstock <rhaubenstock@users.noreply.github.com>
2021-03-24 18:23:50 -0700

    Add handleInput

--- typingTrial.js
+++ typingTrial.js
@@ -1,34 +1,41 @@
 
 
+const keyDownListener = (referenceEl, sampleText) => {
+  // SRP -- 
+  // check what key is pressed -- if it is current char -- green
+  //                              if it is a char
 
+
+};
 
 window.addEventListener("DOMContentLoaded", () => {
-  const referenceEl = document.getElementById("reference");
+  const typedEl = document.getElementById("typed");
+  const currentEl = document.getElementById("current");
+  const toTypeEl = document.getElementById("to-type");
   const inputEl = document.getElementById("input");
   
   const sampleText = "The quick brown fox jumped over the lazy dog.";
 
-  const doneText = "";
-  const currChar = sampleText[0];
-  const nextText = sampleText.slice(1);
+  currentEl.innerText = sampleText[0];
+  toTypeEl.innerText = sampleText.slice(1);
 
+  
 
-  const doneSpan = document.createElement("span");
-  doneSpan.innerText = doneText;
-  doneSpan.classList.add("correct");
+  const handleInput = (key) => {
+    const newSpan = document.createElement("span");
+    newSpan.innerText = key;
+    newSpan.classList.add( key === currentEl.innerText ? "correct" : "error");
+    typedEl.appendChild(newSpan);
+    currentEl.innerText = toTypeEl.innerText[0];
+    console.log(toTypeEl.innerText);
+    toTypeEl.innerText = toTypeEl.innerText.slice(1);
+    console.log(toTypeEl.innerText);
+  }
 
-  const currSpan = document.createElement("span");
-  currSpan.innerText = currChar;
-  currSpan.classList.add("current");
-
-  const nextSpan = document.createElement("span");
-  nextSpan.innerText = nextText;
-  nextSpan.classList.add("next");
-
-  referenceEl.innerHTML="";
-  [doneSpan, currSpan, nextSpan].forEach(el => referenceEl.appendChild(el));
-
-
+  inputEl.addEventListener("keydown", event => {
+    event.preventDefault();
+    if(event.key.length === 1) handleInput(event.key);
+  })
 
 });
 
